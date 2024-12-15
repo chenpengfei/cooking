316b2f1a76948b7b43d3012c6d34ecaf165f6c96
chenpengfei <chenpengfei@lixiang.com>
2024-12-15 10:53:53 +0800

    first commit

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,27 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/hello-ai', async (c) => {
+  const results = await c.env.AI.run('@cf/meta/llama-3.1-8b-instruct', {
+    messages: [
+      { "role": "user", "content": "Say hello world in five different languages, include chinese"}
+    ]
+  })
+  return c.json(results);
+})
+
+app.get('/instruction', async (c) => {
+  const results = await c.env.AI.run('@cf/meta/llama-3.1-8b-instruct', {
+    messages: [
+      { role: "system", content: "you are a professional computer science assistant" },
+      { role: "user", content: "what is WASM?" },
+      { role: "assistant", content: "WASM (WebAssembly) is a binary instruction format that is designed to be a platform-agnostic" },
+      { role: "user", content: "does Python compile to WASM?" },
+      { role: "assistant", content: "No, Python does not directly compile to WebAssembly" },
+      { role: "user", content: "what about Rust?" },
+    ],
+  })
+  return c.json(results);
+})
+
 export default app
